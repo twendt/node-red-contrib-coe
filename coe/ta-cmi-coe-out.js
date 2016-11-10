53180bfb5f427cabb1ff11b9b521b3e51c42c5b0
Timo Wendt <timo@tjwendt.de>
2016-11-10 23:38:36 +0100

    Allow all ports

--- coe/ta-cmi-coe-out.js
+++ coe/ta-cmi-coe-out.js
@@ -15,10 +15,8 @@ module.exports = function(RED) {
 		value = parseInt(msg.payload);
 		buf = new Buffer(14);
 		buf.fill(0);
-		if (port >= 1 && port <= 16) {
-			buf.writeUInt8(nodeid, 0);
-			buf.writeUInt16LE(value, 2);
-		}
+	        buf.writeUInt8(nodeid, 0);
+		buf.writeUInt16LE(value, 2);
 		node.send({payload: buf});
 		node.send({payload: {nodeid: vals[0], port: vals[1], value: msg.payload} });
 	});
