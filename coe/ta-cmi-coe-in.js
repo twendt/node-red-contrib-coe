a1b1085677d8cba13cd98d0b4b5b6aac2b82e2e4
Timo Wendt <timo@tjwendt.de>
2016-11-30 21:28:35 +0000

    Cleanup

--- coe/ta-cmi-coe-in.js
+++ coe/ta-cmi-coe-in.js
@@ -1,56 +1,64 @@
 module.exports = function(RED) {
-    function TaCmiCoeInNode(config) {
-        RED.nodes.createNode(this,config);
-	this.topic_prefix = config.topic_prefix
-        var node = this;
-        this.on('input', function(msg) {
-		var buffer = msg.payload;
-		nodeid = buffer[0];
-		var msgs = [];
-		if (buffer[1] > 0) {
-		  group = buffer[1]-1;
-		  msgtype = "analog";
-		  for (var i=0; i<4 ; i++) {
-		      port = group*4+16+i+1;
-		      if (port >= 49) {
-			msgtype = "digital";
-		      }
-		      value = buffer.readUInt16LE(i*2+2);
-		      //if (msgtype == "digital") {
-		//	  value = (value == 1 ? "ON" : "OFF");
-		      //}
-		      unit = buffer[i+10];
-		      if ( unit == 1) {
-			  value = value / 10;
-		      }
-		      node.send([{payload: {"nodeid": nodeid,
-				 "value": value,
-				 "unit": unit,
-				 "msgtype": msgtype,
-				 "port": port,
-				 "group": group+1,
-				 "index": i
-				 }}, {topic: [node.topic_prefix, nodeid, "/", port].join(""), payload: value}]);
-		  }
-		} else {
-		    msgtype = "digital";
-		    var bits = buffer[2];
-		    for (var i=0;i<16;i++) {
-			var value = bits>>i & 1
-			//value = (value == 1 ? "ON" : "OFF");
-			port = i+1;
-			node.send([{payload: {"nodeid": nodeid,
-				 "value": value,
-				 "unit": 0,
-				 "msgtype": msgtype,
-				 "port": port,
-				 "group": 0,
-				 "index": i
-				 }}, {topic: [node.topic_prefix,nodeid, "/", port].join(""), payload: value}]);
-		    }
-		}
-		return;
-	});
-    }
-    RED.nodes.registerType("coe-in",TaCmiCoeInNode);
+	function TaCmiCoeInNode(config) {
+		RED.nodes.createNode(this, config);
+		this.topic_prefix = config.topic_prefix;
+		var node = this;
+		this.on('input', function(msg) {
+			var buffer = msg.payload;
+			var nodeid = buffer[0];
+			if (buffer[1] > 0) {
+				var group = buffer[1] - 1;
+				var msgtype = "analog";
+				for (var i = 0; i < 4; i++) {
+					var port = group * 4 + 16 + i + 1;
+					if (port >= 49) {
+						msgtype = "digital";
+					}
+					value = buffer.readUInt16LE(i * 2 + 2);
+					var unit = buffer[i + 10];
+					if (unit == 1) {
+						value = value / 10;
+					}
+					node.send([{
+						payload: {
+							"nodeid": nodeid,
+							"value": value,
+							"unit": unit,
+							"msgtype": msgtype,
+							"port": port,
+							"group": group + 1,
+							"index": i
+						}
+					}, {
+						topic: [node.topic_prefix, nodeid, "/", port].join(""),
+						payload: value
+					}]);
+				}
+			} else {
+				msgtype = "digital";
+				var bits = buffer[2];
+				for (var i = 0; i < 16; i++) {
+					var value = bits >> i & 1
+						//value = (value == 1 ? "ON" : "OFF");
+					port = i + 1;
+					node.send([{
+						payload: {
+							"nodeid": nodeid,
+							"value": value,
+							"unit": 0,
+							"msgtype": msgtype,
+							"port": port,
+							"group": 0,
+							"index": i
+						}
+					}, {
+						topic: [node.topic_prefix, nodeid, "/", port].join(""),
+						payload: value
+					}]);
+				}
+			}
+			return;
+		});
+	}
+	RED.nodes.registerType("coe-in", TaCmiCoeInNode);
 }
