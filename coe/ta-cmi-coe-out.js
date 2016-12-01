4fb70bd81b3d22d884af25514629d18d712330e3
Timo Wendt <timo@tjwendt.de>
2016-12-01 19:47:34 +0000

    Cleanup out node

--- coe/ta-cmi-coe-out.js
+++ coe/ta-cmi-coe-out.js
@@ -1,32 +1,40 @@
 module.exports = function(RED) {
-    function TaCmiCoeOutNode(config) {
-        RED.nodes.createNode(this,config);
-	this.topic_prefix = config.topic_prefix
-        var node = this;
-        this.on('input', function(msg) {
-		var re = new RegExp(["^", node.topic_prefix].join(""));
-		var topic = msg.topic.replace(re, "");
-		node.warn(topic);
-		topic = topic.replace(/\/set$/, "");
-		node.warn(topic);
-		var vals = topic.split("\/");
-		nodeid = vals[0];
-		port = parseInt(vals[1]);
-		value = parseInt(msg.payload);
-		buf = new Buffer(14);
-		buf.fill(0);
-                if (port >= 1 && port <= 16) {
-	        	buf.writeUInt8(nodeid, 0);
-			buf.writeUInt16LE(value, 2);
-		} else {
-                        var group = Math.floor(((port-17)/4)+1);
-			buf.writeUInt8(group, 1);
-	        	buf.writeUInt8(nodeid, 0);
-			buf.writeUInt16LE(value, 2);
-		}
-		node.send({payload: buf});
-		node.send({payload: {nodeid: vals[0], port: vals[1], value: msg.payload} });
-	});
-    }
-    RED.nodes.registerType("coe-out",TaCmiCoeOutNode);
+	function TaCmiCoeOutNode(config) {
+		RED.nodes.createNode(this, config);
+		this.topic_prefix = config.topic_prefix;
+		var node = this;
+		this.on('input', function(msg) {
+			var re = new RegExp(["^", node.topic_prefix].join(""));
+			var topic = msg.topic.replace(re, "");
+			node.warn(topic);
+			topic = topic.replace(/\/set$/, "");
+			node.warn(topic);
+			var vals = topic.split("\/");
+			var nodeid = vals[0];
+			var port = parseInt(vals[1]);
+			var value = parseInt(msg.payload);
+			var buf = new Buffer(14);
+			buf.fill(0);
+			if (port >= 1 && port <= 16) {
+				buf.writeUInt8(nodeid, 0);
+				buf.writeUInt16LE(value, 2);
+			} else {
+				var group = Math.floor(((port - 17) / 4) + 1);
+				buf.writeUInt8(group, 1);
+				buf.writeUInt8(nodeid, 0);
+				buf.writeUInt16LE(value, 2);
+			}
+			node.send({
+				payload: buf
+			});
+			node.send({
+				payload: {
+					nodeid: vals[0],
+					port: vals[1],
+					value: msg.payload
+				}
+			});
+		});
+	}
+	RED.nodes.registerType("coe-out", TaCmiCoeOutNode);
 }
